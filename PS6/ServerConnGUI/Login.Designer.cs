4d7bcf31db675550886e646673fa54598c87c426
crsoystr <nicmetz@jackmoves.com>
2014-04-11 14:14:32 -0600

    GIT!!

---
+++ PS6/ServerConnGUI/Login.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ServerConnGUI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IP_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PW_textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // IP_textbox
+            // 
+            this.IP_textbox.Location = new System.Drawing.Point(122, 32);
+            this.IP_textbox.Name = "IP_textbox";
+            this.IP_textbox.Size = new System.Drawing.Size(100, 20);
+            this.IP_textbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please enter in a server IP and password to connect";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IP Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Server Password";
+            // 
+            // PW_textbox
+            // 
+            this.PW_textbox.Location = new System.Drawing.Point(122, 59);
+            this.PW_textbox.Name = "PW_textbox";
+            this.PW_textbox.Size = new System.Drawing.Size(100, 20);
+            this.PW_textbox.TabIndex = 4;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 103);
+            this.Controls.Add(this.PW_textbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IP_textbox);
+            this.Name = "Login";
+            this.Text = "Login to server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IP_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PW_textbox;
+    }
+}
+
---
+++ PS6/ServerConnGUI/Login.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerConnGUI
+{
+    public partial class Login : Form
+    {
+        //variables to be used
+        private bool authenticated = false;
+        private string PW = "";
+        private string IP = "";
+      
+        /// <summary>
+        /// Login GUI start point
+        /// </summary>
+        public Login()
+        {
+        
+            InitializeComponent();
+
+            //create keypress handlers for each text box
+            this.IP_textbox.KeyPress += new KeyPressEventHandler(waitForEnter);
+            this.PW_textbox.KeyPress += new KeyPressEventHandler(waitForEnter);
+
+        }
+
+        /// <summary>
+        /// Wait for user to enter username and password
+        /// </summary>
+        /// <param name="sender">object event is coming from</param>
+        /// <param name="e"></param>
+        private void waitForEnter(object sender, KeyPressEventArgs e)
+        {
+            //if keycode is enter
+           if(e.KeyChar == (char)13)
+           {
+               //if the IP isn't entered error box
+               if(IP_textbox.Text.Length < 1)
+               {
+                   MessageBox.Show("Please enter a valid server IP address");
+               }
+               //if PW text box has no value error box
+               else if (PW_textbox.Text == "")
+               {
+                   MessageBox.Show("Please enter a password to connect to the server");
+               }
+               //if both tb.text field have values, save them as variables
+               else
+               {
+                   PW = PW_textbox.Text;
+                   IP = IP_textbox.Text;
+                    
+                   //hide login window
+                   this.Hide();
+                   //open new spreadsheet
+                   new SpreadsheetGUI.SpreadsheetGUIForm().Show();
+               }
+           }
+        }
+    }
+}
